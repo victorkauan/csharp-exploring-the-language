1baf1ded0638e0b5b789af4ed31a32476a2a4c24
Victor Kauan <victorkauan.dev@gmail.com>
2023-04-22 15:10:26 -0300

    Create VariableAssignments project.

---
+++ 6-VariableAssignments/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Project 06 - Variable Assignments");
+
+        int age = 30;
+        int annaAge = age;
+
+        Console.WriteLine(age);
+        Console.WriteLine(annaAge);
+
+        age = 25;
+        
+        Console.WriteLine(age);
+        Console.WriteLine(annaAge);
+
+        Console.WriteLine("Press return to close...");
+        Console.ReadLine();
+    }
+}
