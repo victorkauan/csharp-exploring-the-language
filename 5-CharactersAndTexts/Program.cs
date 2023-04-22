07259c00d902952c62a458bab235cc2b4a9bb608
Victor Kauan <victorkauan.dev@gmail.com>
2023-04-22 14:54:04 -0300

    Create CharactersAndTexts project.

---
+++ 5-CharactersAndTexts/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Project 05 - Characters and Texts");
+
+        char letter = 'a';
+        Console.WriteLine(letter);
+
+        letter = (char)65;
+        Console.WriteLine(letter);
+
+        letter = (char)(65 + 1);
+        Console.WriteLine(letter);
+        
+        letter = (char)(4 * 12 / 2);
+        Console.WriteLine(letter);
+
+        letter = (char)(letter + 1);
+        Console.WriteLine(letter);
+
+        int value = 92;
+        letter = (char)(value - 10);
+        Console.WriteLine(letter);
+
+        letter = ' ';
+        Console.WriteLine(letter);
+
+        string firstSentence = "Alura - Online Technology Courses ";
+        Console.WriteLine(firstSentence + 2023);
+
+        string courses = @"Available courses:
+- Go
+- C#
+- Python
+- Java";
+        Console.WriteLine(courses);
+
+        string emptyString = "";
+        Console.WriteLine(emptyString);
+
+        Console.WriteLine("Press return to close...");
+        Console.ReadLine();
+    }
+}
