361cc19dbdabd64ddc83d1fe90583ce0d6dabd85
Victor Kauan <victorkauan.dev@gmail.com>
2023-04-22 09:44:39 -0300

    Add close command in HelloWorld project.

--- 1-HelloWorld/Program.cs
+++ 1-HelloWorld/Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, world!");
+        Console.WriteLine("Press return to close this program...");
+        Console.ReadLine();
     }
 }
